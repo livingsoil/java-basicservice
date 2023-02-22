2f7c56a4d9b318ef934b5230d0ca69debd8ceb5a
Ammon King <ammon.king@phoenix.edu>
2023-02-21 20:07:43 -0700

    initial commit

---
+++ src/main/java/com/example/basicservice/BasicserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicserviceApplication.class, args);
+	}
+
+}
